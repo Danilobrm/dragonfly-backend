2b76d17471283d8f605ae2439a4b77f730efa8db
danilobrm <danilobr.web@gmail.com>
2023-08-05 16:31:04 -0300

    first commit

---
+++ src/main/java/com/example/dragonflybackend/DragonflyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.dragonflybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DragonflyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DragonflyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dragonflybackend/DragonflyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dragonflybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DragonflyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
